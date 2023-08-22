0779241f451033ab0d46df9052dca140373c1895
Victor Barra <victorbarrapereira@gmail.com>
2023-08-22 11:04:09 -0300

    aula 2

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,8 +1,19 @@
+import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho"
+import Menu from "../../componentes/Menu"
+import style from './paginaInicial.module.scss'
 
 export default function PaginaInicial(){
     return(
-        <Cabecalho />
-    )
+        <>    
+            <Cabecalho />
+            <main>
+                <section className={style.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+            </main>
+        </>
+    );
 }
    
